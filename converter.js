3141945e8092d187d537ba21b5eabaeed43be151
DESKTOP-RUCEG1P\Timothy <timothyouano@gmail.com>
2018-02-20 16:14:27 +0800

    Fixed bug converter not converting past million

--- converter.js
+++ converter.js
@@ -27,19 +27,19 @@ $(document).ready(function(){
           $(this).attr('data-original-title', 'Price is too large please enter a smaller number').tooltip('show');
         }
         else if(billion > 1){
-          var million = Math.floor(thousand % 1000);
+          var millionnorm = Math.floor(thousand % 1000);
           var thousandnorm = Math.floor(thousand % 100);
           var lakh = Math.floor(hundred % 100);
           var crore = Math.floor(million % 100);
           var thousandind = Math.floor(thousand % 1000);
 
-          million = flag1!=0?flag1 + ' Million':'';
-          thousandnorm = flag!=0?flag2 + ' Thousand':'';
-          lakh = flag!=0?flag3 + ' Lakh ':'';
-          crore = flag4!=0?flag4 + ' Crore ':'';
-          thousandind = flag5!=0?flag5 + ' Thousand':'';
+          millionnorm = millionnorm!=0?millionnorm + ' Million':'';
+          thousandnorm = thousandnorm!=0?thousandnorm + ' Thousand':'';
+          lakh = lakh!=0?lakh + ' Lakh ':'';
+          crore = crore!=0?crore + ' Crore ':'';
+          thousandind = thousandind!=0?thousandind + ' Thousand':'';
 
-          var data = Math.floor(billion) + ' Billion ' + flag1 + flag5 + separator + Math.floor(million / 1000) + ' Arab ' + flag4 + flag3 + flag2;
+          var data = Math.floor(billion) + ' Billion ' + millionnorm + ' ' + thousandind + separator + Math.floor(million / 1000) + ' Arab ' + crore + lakh + thousandnorm;
           $(this).attr('data-original-title', data).tooltip('show').tooltip('show');
         }
         else if(million > 1){
@@ -47,16 +47,16 @@ $(document).ready(function(){
           var lakh = Math.floor(hundred % 100);
           var thousandind = Math.floor(thousand % 1000);
 
-          thousandnorm = flag1!=0?flag1 + ' Thousand':'';
-          lakh = flag2!=0?flag2 + ' Lakh ':'';
-          thousandind = flag!=0?flag3 + ' Thousand':'';
-          var data = Math.floor(million) + ' Million ' + flag3 + '<br/>' + separator + Math.floor(hundred / 100) + ' Crore ' + flag2 + flag1;
+          thousandnorm = thousandnorm!=0?thousandnorm + ' Thousand':'';
+          lakh = lakh!=0?lakh + ' Lakh ':'';
+          thousandind = thousandind!=0?thousandind + ' Thousand':'';
+          var data = Math.floor(million) + ' Million ' + thousandind + '<br/>' + separator + Math.floor(hundred / 100) + ' Crore ' + lakh + thousandnorm;
 
           if(hundred * 100 >= 10000){
               $(this).attr('data-original-title', data).tooltip('show');
           }
           else{
-              $(this).attr('data-original-title', Math.floor(million) + ' Million ' + flag3 + '<br/>' + separator + Math.floor(hundred) + ' Lakh ' + flag1).tooltip('show');
+              $(this).attr('data-original-title', Math.floor(million) + ' Million ' + thousandind + '<br/>' + separator + Math.floor(hundred) + ' Lakh ' + thousandnorm).tooltip('show');
           }
         }
         else if(hundred > 1){
